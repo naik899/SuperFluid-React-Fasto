952c85843e55e19a1e9e676f9927b7fff3431fcc
Ravindra Naik <naik899@users.noreply.github.com>
2021-08-05 00:16:03 +0530

    changes
    changes

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -69,23 +69,7 @@ function Header ({onNote}) {
 					as="li"
 					className="nav-item dropdown notification_dropdown "
 				  >
-					<Dropdown.Toggle
-					  variant=""
-					  as="a"
-					  className="nav-link bell bell-link i-false c-pointer"
-					  onClick={() => onNote()}
-					>
-						<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M2.23779 10.2492L4.66679 11.7064V8.30554L2.23779 10.2492Z" fill="#67636D"/>
-							<path d="M1.1665 12.327V23.3334C1.16852 23.8531 1.28817 24.3656 1.5165 24.8325L9.20134 17.15L1.1665 12.327Z" fill="#67636D"/>
-							<path d="M26.4832 24.8325C26.7115 24.3656 26.8311 23.8531 26.8332 23.3334V12.327L18.7983 17.15L26.4832 24.8325Z" fill="#67636D"/>
-							<path d="M23.3335 8.30554V11.7064L25.7625 10.2492L23.3335 8.30554Z" fill="#67636D"/>
-							<path d="M21.0492 13.0772C21.024 12.998 21.0076 12.9162 21.0002 12.8334V7.00004C21.0002 6.69062 20.8773 6.39388 20.6585 6.17508C20.4397 5.95629 20.1429 5.83337 19.8335 5.83337H8.16684C7.85742 5.83337 7.56067 5.95629 7.34188 6.17508C7.12309 6.39388 7.00017 6.69062 7.00017 7.00004V12.8334C6.99274 12.9162 6.97631 12.998 6.95117 13.0772L14.0002 17.3064L21.0492 13.0772Z" fill="#67636D"/>
-							<path d="M17.3262 3.50003L14.7292 1.4222C14.5222 1.25653 14.2651 1.16626 14 1.16626C13.7349 1.16626 13.4777 1.25653 13.2708 1.4222L10.6738 3.50003H17.3262Z" fill="#67636D"/>
-							<path d="M16.7358 18.3855L14.6008 19.6688C14.4194 19.7778 14.2117 19.8354 14 19.8354C13.7883 19.8354 13.5806 19.7778 13.3991 19.6688L11.2641 18.3855L3.16748 26.4833C3.63438 26.7117 4.14691 26.8313 4.66665 26.8333H23.3333C23.853 26.8313 24.3656 26.7117 24.8325 26.4833L16.7358 18.3855Z" fill="#67636D"/>
-						</svg>
-						<span className="badge light text-white bg-primary rounded-circle">6</span>
-					</Dropdown.Toggle>
+				
 				  </Dropdown>
              
               <Dropdown as="li" className="nav-item dropdown header-profile">
@@ -99,8 +83,8 @@ function Header ({onNote}) {
                 >
                   <img src={profile} width={20} alt="" />
                   <div className="header-info">
-                    <span>Johndoe</span>
-                    <small>Super Admin</small>
+                    <span>Gyan Lakshmi</span>
+                    <small>Borrower</small>
                   </div>
                 </Dropdown.Toggle>
 
