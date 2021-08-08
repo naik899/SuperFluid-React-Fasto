c4a9f812af41b2928f40656222dd6d70877b9a96
vikaspachori <vikas_pachori@yahoo.com>
2021-08-08 14:24:28 +0530

    fixed system steaming

--- src/jsx/components/Forms/ReduxWizard/StreamAdjustmentPage.js
+++ src/jsx/components/Forms/ReduxWizard/StreamAdjustmentPage.js
@@ -8,111 +8,108 @@ import SuperfluidSDK from "@superfluid-finance/js-sdk";
 import Web3 from "web3";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Loan  from "../../../../contracts/Loan.json";
-
+import Loan from "../../../../contracts/Loan.json";
 
 const loanApplications = function() {
-  
+  debugger;
   let applications = [];
-  applications = JSON.parse(localStorage.getItem("loanApplications"));
-  if(applications != null)
-  {
+
+  if (applications != null) {
     let result = [];
-    applications.forEach(element => {
+    applications.forEach((element) => {
       result.push(element.loanId);
     });
 
     console.log(result);
   }
-}
+};
 
-const loanApplication = ["22622", "test2"];
+const applications = JSON.parse(localStorage.getItem("loanApplications"));
+const walletAddress = (localStorage.getItem("walletAddress"));
+const loanApplication = applications
+  .filter((d) => d.borrower == walletAddress)
+  .map((d) => d.loanId);
+localStorage.setItem("steamids", JSON.stringify(loanApplication));
+// const loanApplication = JSON.parse(localStorage.getItem("steamids"));
 
 const loanSelector = ({ input, meta: { touched, error } }) => (
-
-
-
   <div>
     <select className="form-control" {...input}>
       <option value="">Select a Loan Account</option>
-      {loanApplication.map(val => <option value={val} key={val}>{val}</option>)}
+      {loanApplication.map((val) => (
+        <option value={val} key={val}>
+          {val}
+        </option>
+      ))}
     </select>
     {touched && error && <span>{error}</span>}
   </div>
 );
 
-const StreamAdjustmentPage = props => {
+const StreamAdjustmentPage = (props) => {
   const { handleSubmit } = props;
-
-  
 
   return (
     <>
-     <ToastContainer
-                  position="top-right"
-                  autoClose={5000}
-                  hideProgressBar={false}
-                  newestOnTop
-                  closeOnClick
-                  rtl={false}
-                  pauseOnFocusLoss
-                  draggable
-                  pauseOnHover
-                />
-		<form onSubmit={handleSubmit}>
-			<div className="validate-redux-form row">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <form onSubmit={handleSubmit}>
+        <div className="validate-redux-form row">
+          <div className="col-sm-8">
+            <div className="form-group">
+              <Field name="loanSelector" component={loanSelector} />
+            </div>
+          </div>
+          <div className="col-sm-4"></div>
 
+          <div className="col-sm-8">
+            <Field
+              name="loanTenure"
+              type="text"
+              component={renderField}
+              label="Loan Tenure"
+            />
+          </div>
+          <div className="col-sm-4"></div>
 
-                <div className="col-sm-8">
-                    <div className="form-group">
-                        <Field name="loanSelector" component={loanSelector} />
-                    </div>
-                    
-                </div>
-                <div className="col-sm-4">
-                    
-                    
-                </div>
+          <div className="col-sm-8">
+            <Field
+              name="flowRateTenure"
+              type="text"
+              component={renderField}
+              label="Loan deduction frequency in days"
+            />
+          </div>
+          <div className="col-sm-4"></div>
 
-          
-				<div className="col-sm-8">
-					<Field name="loanTenure" type="text" component={renderField} label="Loan Tenure"/>
-				</div>	
-				<div className="col-sm-4">
-                    
-                    
-                </div>
-
-                <div className="col-sm-8">
-					<Field name="flowRateTenure" type="text" component={renderField} label="Loan deduction frequency in days"/>
-				</div>	
-				<div className="col-sm-4">
-                    
-                    
-                </div>
-
-
-
-				
-
-				<div className="col-sm-12 mt-4 ">
-					<button  type="submit" className="next btn btn-primary"> Submit</button>
-				</div>
-			</div>
-		</form>
-  </>
+          <div className="col-sm-12 mt-4 ">
+            <button type="submit" className="next btn btn-primary">
+              {" "}
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
   );
 };
 
 export default compose(
-	connect((state, props) => {
+  connect((state, props) => {
     return {
-		initialValues: {
-		
-			flowRateTenure: 1,
-			loanTenure: "60"
-
-		}
+      initialValues: {
+        flowRateTenure: 1,
+        loanTenure: "60",
+      },
     };
   }),
   reduxForm({
@@ -120,72 +117,81 @@ export default compose(
     destroyOnUnmount: false, //        <------ preserve form data
     forceUnregisterOnUnmount: true, // <------ unregister fields on unmount
     validate,
-	onSubmit:async () =>{
-	
+    onSubmit: async () => {
+      let walletAddress = localStorage.getItem("walletAddress");
+      let loanId = document.getElementsByName("loanSelector")[0].value;
+      let loanTenure = document.getElementsByName("loanTenure")[0].value;
+      let flowRateTenure = document.getElementsByName("flowRateTenure")[0]
+        .value;
 
-    let walletAddress = localStorage.getItem("walletAddress");
-    let loanId = document.getElementsByName("loanSelector")[0].value;
-    let loanTenure = document.getElementsByName("loanTenure")[0].value;
-    let flowRateTenure = document.getElementsByName("flowRateTenure")[0].value;
+      const sf = new SuperfluidSDK.Framework({
+        web3: new Web3(window.ethereum),
+      });
+      await sf.initialize();
 
+      const carol = sf.user({
+        address: walletAddress,
+        token: "0x5943F705aBb6834Cad767e6E4bB258Bc48D9C947",
+      });
 
-    const sf = new SuperfluidSDK.Framework({
-      web3: new Web3(window.ethereum),
-    });
-    await sf.initialize();
+      let loanInfo = [];
+      loanInfo = JSON.parse(localStorage.getItem("loanApplications"));
+      loanInfo = await loanInfo.filter((s) => s.loanId == loanId);
 
-    const carol = sf.user({
-      address: walletAddress,
-      token: "0x5943F705aBb6834Cad767e6E4bB258Bc48D9C947",
-    });
-  
+      let flowRate = 802469;
 
-    let loanInfo = []
-    loanInfo = JSON.parse(localStorage.getItem("loanApplications"));
-    loanInfo = await loanInfo.filter(s=> s.loanId == loanId);
+      let result = await carol.flow({
+        recipient: loanInfo[0].lender,
+        flowRate: "000000000802469",
+      });
 
-    let flowRate = 802469;
+      let web3 = new Web3(Web3.givenProvider);
+      const coinContract = new web3.eth.Contract(
+        Loan.abi,
+        loanInfo[0].contractAddress
+      );
 
-    let result = await carol.flow({
-      recipient: loanInfo[0].lender,
-      flowRate: "000000000802469",
-    });
+      let streamAmount = 2441406250;
 
-    let web3 = new Web3(Web3.givenProvider);
-    const coinContract = new web3.eth.Contract(Loan.abi, loanInfo[0].contractAddress);
+      let result1 = coinContract.methods.updateLoan(
+        2441406250,
+        60,
+        streamAmount
+      );
+      const transactionParameters = {
+        from: walletAddress, // must match user's active address.
+        gas: "0x76c0", // 30400
+        gasPrice: "0x9184e72a000",
+      };
+      let result2 = await result1
+        .send(transactionParameters, function(error, transactionHash) {
+          console.log("Tx has:" + transactionHash);
+        })
+        .on("error", function(error) {
+          console.log(error);
+        })
+        .on("transactionHash", function(transactionHash) {
+          console.log("New Tx: " + transactionHash);
+        })
+        .on("receipt", function(receipt) {
+          console.log("Received");
+          console.log(receipt.contractAddress); // contains the new contract address
+        })
+        .on("confirmation", function(confirmationNumber, receipt) {
+          console.log(
+            "Confirmation:" + confirmationNumber + " , Receipt: " + receipt
+          );
+        });
 
-    let streamAmount = 2441406250;
-    
-    let result1 =   coinContract.methods.updateLoan(2441406250,60, streamAmount);
-    const transactionParameters = {
-    
-      from: walletAddress, // must match user's active address.
-      gas: '0x76c0', // 30400
-      gasPrice: '0x9184e72a000'
-    };
-    let result2 = await result1.send( transactionParameters, function(error, transactionHash){ console.log("Tx has:" + transactionHash); })
-                            .on('error', function(error){ console.log(error); })
-                            .on('transactionHash', function(transactionHash){ console.log("New Tx: " + transactionHash) })
-                            .on('receipt', function(receipt){
-                              console.log("Received");
-                               console.log(receipt.contractAddress) // contains the new contract address
-                            })
-                            .on('confirmation', function(confirmationNumber, receipt){ console.log("Confirmation:" + confirmationNumber + " , Receipt: " + receipt
-                            )});
-  
-
-
-    toast.success("Stream Adjustment successfully completed", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-    });
-
-    
-	},
-    enableReinitialize: true
+      toast.success("Stream Adjustment successfully completed", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    },
+    enableReinitialize: true,
   })
 )(StreamAdjustmentPage);
